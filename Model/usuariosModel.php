97787e492af37a02b49ac3697d3d25e87fbf6526
Francisco Larrosa <franciscolarrosa22@gmail.com>
2021-10-10 19:46:18 -0300

    update

--- Model/usuariosModel.php
+++ Model/usuariosModel.php
@@ -7,23 +7,33 @@ class usuariosModel {
     private $db;
 
 
-    function __construct()
+    public function __construct()
     {
         $this->db = new PDO('mysql:host=localhost;'.'dbname=libreria;charset=utf8', 'root', '');
     }
 
 // Registro usuario
-function registration($email,$user_password){
+public function registration($email,$user_password){
     $obtener = $this->db->prepare("INSERT INTO usuarios(email,user_password) VALUES(?,?)");
     $obtener->execute(array($email,$user_password));
 }
 
 
 // Obtener user
-function getUser($email){
+public function getUser($email){
     $obtener = $this->db->prepare("SELECT * FROM usuarios WHERE email=? ");
     $obtener->execute(array($email));
     $user = $obtener->fetch(PDO::FETCH_OBJ);
     return $user;
 }
+
+// Obtener lista de usuarios 
+public function listaCompleta(){
+    $obtener = $this->db->prepare("SELECT * FROM usuarios");
+    $obtener->execute();
+    $lista = $obtener->fetchAll(PDO::FETCH_OBJ);
+    return $lista;
+}
+
+
 }
